acb52aa48ac3661f006720b341412404db1b50dd
Jason Lengstorf <jason@lengstorf.com>
2023-07-29 22:19:13 -0700

    feat: add empty state

--- src/components/messages.tsx
+++ src/components/messages.tsx
@@ -4,6 +4,20 @@ import { api } from '../../convex/_generated/api';
 
 import styles from './messages.module.css';
 
+const Empty = () => {
+	return (
+		<div className={styles.empty}>
+			<h1>Ready to start logging?</h1>
+			<p>
+				Text a photo and any details or description you want to include about it
+				to:
+				<a href="sms:+18145267822">+1 814-526-7822</a>
+				Send your first text to see it logged here!
+			</p>
+		</div>
+	);
+};
+
 export const Messages = () => {
 	const { user } = useUser();
 
@@ -11,25 +25,29 @@ export const Messages = () => {
 	const sender = user!.primaryPhoneNumber!.phoneNumber;
 
 	// load all messages from the currently logged in user
-	const messages = useQuery(api.messages.get, { sender });
+	const messages = useQuery(api.messages.get, { sender }) || [];
 
 	return (
 		<section className={styles.wrapper}>
-			<ul className={styles.messages}>
-				{messages?.map(({ _id, _creationTime, text, image }) => {
-					return (
-						<li key={_id} className={styles.message}>
-							{image && image.url ? (
-								<img className={styles.image} src={image.url} alt={text} />
-							) : null}
-							<p className={styles.text}>{text}</p>
-							<p className={styles.meta}>
-								Posted {new Date(_creationTime).toLocaleString()}
-							</p>
-						</li>
-					);
-				})}
-			</ul>
+			{messages.length > 0 ? (
+				<ul className={styles.messages}>
+					{messages.map(({ _id, _creationTime, text, image }) => {
+						return (
+							<li key={_id} className={styles.message}>
+								{image && image.url ? (
+									<img className={styles.image} src={image.url} alt={text} />
+								) : null}
+								<p className={styles.text}>{text}</p>
+								<p className={styles.meta}>
+									Posted {new Date(_creationTime).toLocaleString()}
+								</p>
+							</li>
+						);
+					})}
+				</ul>
+			) : (
+				<Empty />
+			)}
 		</section>
 	);
 };
